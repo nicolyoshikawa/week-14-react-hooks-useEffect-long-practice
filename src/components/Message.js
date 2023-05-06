3290becfc2b622d8b4451614338d8949113a4261
Nicol Yoshikawa <nicolyoshikawa@Nicols-MBP.attlocal.net>
2023-05-06 14:14:03 -0700

    completed

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,13 +1,42 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
-function Message({ size }) {
+function Message({ size, featherCount }) {
+  const [sizeClass, setSizeClass] = useState('');
+  const [message, setMessage] = useState('');
+
   useEffect(() => {
     console.log('Message', size);
-  }, [size])
+    let cname = '';
+    switch (size) {
+      case 'm':
+        cname = 'medium';
+        break;
+      case 'l':
+        cname = 'large';
+        break;
+      case 'xl':
+        cname = 'xlarge';
+        break;
+      default:
+        cname = 'small';
+        break;
+    }
+    setSizeClass(cname);
+  }, [size]);
+
+  useEffect(() =>{
+    if (featherCount <= 0)
+      setMessage('Oh my! Your bird is naked!');
+    else if (featherCount >= 10) {
+      setMessage('Full turkey!');
+    } else {
+      setMessage('Coming along...');
+    }
+  }, [featherCount]);
 
   return (
-    <div className="message medium">
-      (Oh my! Your bird is naked!)
+    <div className={`message ${sizeClass}`}>
+      {message}
     </div>
   );
 };
